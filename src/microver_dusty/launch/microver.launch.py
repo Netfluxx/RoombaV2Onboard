a8b90aca928188a991ce004b4d439b2e252c0624
arno <arno.blan334@gmail.com>
2024-08-12 20:59:29 +0200

    added a prototype for a motor controller that reads the /cmd_vel topic with some kinematics for skid steer and sends them to the arduinos

--- src/microver_dusty/launch/microver.launch.py
+++ src/microver_dusty/launch/microver.launch.py
@@ -1,49 +1,75 @@
-import launch
-from launch.substitutions import Command, LaunchConfiguration
-from launch_ros.parameter_descriptions import ParameterValue
-import launch_ros
 import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package='microver_dusty').find('microver_dusty')
-    default_model_path = os.path.join(pkg_share, 'models/microver.urdf.xacro')
-    default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
 
-    robot_state_publisher_node = launch_ros.actions.Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': ParameterValue(Command(['xacro ', LaunchConfiguration('model')]), value_type=str)}]
-    )
-    joint_state_publisher_node = launch_ros.actions.Node(
+    # Set the path to different files and folders.
+    pkg_share = FindPackageShare(package='microver_dusty').find('microver_dusty')
+    default_model_path = os.path.join(pkg_share, 'models/microver.urdf.xacro')
+    robot_name_in_urdf = 'microver'
+    
+    # Launch configuration variables specific to the robot
+    model = LaunchConfiguration('model')
+    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    # Declare the launch arguments  
+    declare_model_path_cmd = DeclareLaunchArgument(
+        name='model', 
+        default_value=default_model_path, 
+        description='Absolute path to robot URDF file')
+    
+    declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
+        name='use_robot_state_pub',
+        default_value='True',
+        description='Whether to start the robot state publisher')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='False',
+        description='Use simulation (Gazebo) clock if true')
+
+    # Publish the joint state values for the non-fixed joints in the URDF file.
+    start_joint_state_publisher_cmd = Node(
+        condition=UnlessCondition(LaunchConfiguration('gui')),
         package='joint_state_publisher',
         executable='joint_state_publisher',
-        name='joint_state_publisher',
-        parameters=[{'robot_description': ParameterValue(Command(['xacro ', default_model_path]), value_type=str)}],
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
-    )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
-    )
-    rviz_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
+        name='joint_state_publisher')
+
+    # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
+    start_robot_state_publisher_cmd = Node(
+        condition=IfCondition(use_robot_state_pub),
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'use_sim_time': use_sim_time, 
+                     'robot_description': Command(['xacro ', model])}],
+        arguments=[default_model_path])
+
+    # Static transform from base_link to lidar_frame (Example)
+    static_transform_publisher_cmd = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_transform_publisher',
         output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
+        arguments=['0.2', '0.0', '0.1', '0', '0', '0', 'base_link', 'lidar_frame']
     )
 
-    return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
-                                             description='Flag to enable joint_state_publisher_gui'),
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                             description='Absolute path to robot urdf file'),
-        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-                                             description='Absolute path to rviz config file'),
-        joint_state_publisher_node,
-        joint_state_publisher_gui_node,
-        robot_state_publisher_node,
-        rviz_node
-    ])
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_model_path_cmd)
+    ld.add_action(declare_use_robot_state_pub_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+
+    # Add any actions
+    ld.add_action(start_joint_state_publisher_cmd)
+    ld.add_action(start_robot_state_publisher_cmd)
+    ld.add_action(static_transform_publisher_cmd)
+
+    return ld
