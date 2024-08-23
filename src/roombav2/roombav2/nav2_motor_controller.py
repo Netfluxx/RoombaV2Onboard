7d2e8e9c3d22872b7af223f4cacec8386bef7c3b
Arno Laurie <87619843+Netfluxx@users.noreply.github.com>
2024-08-23 17:14:50 +0200

    removed the printed hwid when connected to the arduino serial port

--- src/roombav2/roombav2/nav2_motor_controller.py
+++ src/roombav2/roombav2/nav2_motor_controller.py
@@ -44,12 +44,12 @@ class Nav2MotorControl(Node):
         for port in ports:
             if 'USB' in port.description and '1A86:7523' in port.hwid: #master arduino hwid number
                 try:
-                    print(port.hwid)
+                    #print(port.hwid)
                     serial_port = serial.Serial(port.device, 9600, timeout=1)
-                    self.get_logger().info(f"Connected to serial port: {port.device}")
+                    self.get_logger().info(f"CONNECTED to serial port: {port.device}")
                     return serial_port
                 except serial.SerialException as e:
-                    self.get_logger().error(f"Failed to open serial port {port.device}: {e}")
+                    self.get_logger().error(f"FAILED to open serial port {port.device}: {e}")
         return None
 
     def compute_kinematics(self, lin_vel, ang_vel):
