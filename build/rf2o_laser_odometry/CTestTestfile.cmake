ff3d014939d3b2f64da0f8191a318022be5cb7d6
arno <arno.blan334@gmail.com>
2024-08-27 17:54:59 +0200

    added sllidar_ros2 as just files not a submodule so i can edit it. this runs on raspberry pi4b

--- build/rf2o_laser_odometry/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arno/RoombaV2Onboard/src/rf2o_laser_odometry
-# Build directory: /home/arno/RoombaV2Onboard/build/rf2o_laser_odometry
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
